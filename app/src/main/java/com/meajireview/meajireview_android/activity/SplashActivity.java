5a8fd802835f3bf50e0302b041d7e95362ad06a7
kang <seun2354@naver.com>
2016-10-10 15:30:35 +0900

    splash 적용. 이미지는 아직 넣지 않음.

---
+++ app/src/main/java/com/meajireview/meajireview_android/activity/SplashActivity.java
@@ -0,0 +1,29 @@
+package com.meajireview.meajireview_android.activity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import com.meajireview.meajireview_android.R;
+
+/**
+ * Created by TOMO on 2016-10-07.
+ */
+
+public class SplashActivity extends Activity {
+    int SPLASH_TIME=2000;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                overridePendingTransition(0, android.R.anim.fade_in);
+                startActivity(new Intent(SplashActivity.this, MainActivity.class));
+                finish();
+            }
+        }, SPLASH_TIME);
+    }
+}
