f4e00baf5e0781c66fda17317971bd692dc372c4
Sujin <green41255@nate.com>
2016-11-07 00:44:33 +0900

    ShopList 관련 파일 생성

--- app/src/main/java/com/meajireview/meajireview_android/item/ShopInfo.java
+++ app/src/main/java/com/meajireview/meajireview_android/item/ShopInfo.java
@@ -5,4 +5,30 @@ package com.meajireview.meajireview_android.item;
  */
 
 public class ShopInfo {
+
+    String shopName;
+    String shopCall;
+    String shopRating;
+
+    public String getShopName()
+    {
+        return shopName;
+    }
+
+    public String getShopCall()
+    {
+        return shopCall;
+    }
+
+    public String getShopRating()
+    {
+        return shopRating;
+    }
+
+    public ShopInfo(String shopName, String shopCall, String shopRating)
+    {
+        this.shopName = shopName;
+        this.shopCall = shopCall;
+        this.shopRating = shopRating;
+    }
 }
